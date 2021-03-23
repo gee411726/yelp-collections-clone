ab816190fb97cb9773d15db3e2a99c4eebb31811
gee411726 <gee411726@gmail.com>
2021-03-22 23:47:00 -0400

    changed changed app.get(...) to go up a dir

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../src/index.js"));
 });
 
 app.listen(port, () => {
