74dfd3f9d3f4f4934ea6930978030f08d05b352c
gee411726 <gee411726@gmail.com>
2021-03-23 10:47:06 -0400

    fixing path join route in server.js

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ app.use('/api/collections', collectionsRouter);
 // Heroku assigns NODE_ENV to production, serving production ready React app and re-routing client-side routes to index.html
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html')); // potentially invert (have client in folder,  server outside...)
   });
 }
 
